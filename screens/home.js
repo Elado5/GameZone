b156e95d7fa3c3a17c475bb10807c9e5e482fdb4
Elad Or <elad725@gmail.com>
2021-05-24 21:32:49 +0300

    Changed to passing info from home to details screen

--- screens/home.js
+++ screens/home.js
@@ -1,18 +1,29 @@
-import React from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native'
+import React, {useState} from 'react'
+import { StyleSheet, View, Text, Button, FlatList, TouchableOpacity} from 'react-native'
 import {globalStyles} from '../styles/global'
+
 
 export default function Home({navigation}) {
 
-    const pressHandler = () => {
-        //navigation.navigate('Details') does the same thing
-        navigation.push('Details')
-    }
 
+const [reviews, setReviews] = useState([
+    {title: 'Naruto Shippuden', rating: 5, body: 'Rasengan!', key: '1'},
+    {title: 'Bleach', rating: 5, body:'Getsuga Tenshou', key: '2'},
+    {title: 'One Piece', rating: 5, body:'Pirate King', key: '3'}
+])
+
+//item is individual review
+//we send in navigate the item data
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home Screen</Text>
-            <Button title='go to review details' onPress={pressHandler} />
+            <FlatList 
+            data={reviews}
+            renderItem={({item}) => (
+                <TouchableOpacity onPress ={() => navigation.navigate('Details', item )}>
+                    <Text style={globalStyles.titleText}> {item.title}</Text>
+                </TouchableOpacity>
+            )}
+            />            
         </View>
     )
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { StyleSheet, View, Text, Button} from 'react-native'
 import {globalStyles} from '../styles/global'
 
-export default function ReviewDetails({navigation}) {
+export default function ReviewDetails({route, navigation}) {
+
+    const {title, body, rating} = route.params
 
     const pressHandler = () => {
         navigation.goBack();
@@ -10,8 +12,9 @@ export default function ReviewDetails({navigation}) {
 
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Review Screen</Text>
-            <Button style={globalStyles.navButton} title="back to home screen" onPress={pressHandler}/>
+            <Text style={globalStyles.titleText}> {title} </Text>
+            <Text style={globalStyles.titleText}> {body} </Text>
+            <Text style={globalStyles.titleText}> {rating} </Text>
         </View>
     )
 }
