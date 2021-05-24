7cab84fca3a90d3c634957a0179d842f2fc7ca55
Elad Or <elad725@gmail.com>
2021-05-24 19:06:25 +0300

    added global styles and fonts, imported react navigation 5

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
+//import react stuff
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react'
+//import screens
+import Home from './screens/home'
+import About from './screens/about'
+import ReviewDetails from './screens/reviewDetails'
+//react navigation
+import { NavigationContainer } from '@react-navigation/native';
+//
+import AppLoading from 'expo-app-loading'
+//so we can use external fonts
+import * as Font from 'expo-font'
+
+
+const getFonts = () => Font.loadAsync({
+    'nunito-regular' : require('./assets/fonts/nunito-regular.ttf'),
+    'nunito-bold': require('./assets/fonts/nunito-bold.ttf')
+  })
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  //if the fonts are already loaded start regularly
+  if(fontsLoaded) {
+    return (
+      <Home />
+    )
+  }
+  //load the fonts and then start
+  else {
+    return(
+    <AppLoading 
+      startAsync={getFonts}
+      onError = {console.warn}
+      onFinish={() => setFontsLoaded(true)}
+    />
+    )
+  }
+
+  
+}
